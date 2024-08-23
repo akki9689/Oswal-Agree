e5c655987bd35ba11bd6052a3985a590f99c1849
Mushafiya <kmushafiya003@gmail.com>
2024-08-23 22:09:58 +0530

    Added colours and font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,106 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-  
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // Include your source files
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-        screens : {
 
-        'dxs' : '300px',
-        // => @media (min-width: 300px) { ... }
-  
-        'xxs' : '400px',
-        // => @media (min-width: 400px) { ... }
-  
-        'xsm' : '458px',
-        // => @media (min-width: 400px) { ... }
-  
-        'xs' : '500px',
-        // => @media (min-width: 500px) { ... }
-  
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
+    fontFamily: {
+      "open-sans": ["Open Sans", "sans-serif"],
+    },
+
+    colors: {
+      transparent: "#ffffff00",
+
+      "white-shade": {
+        100: "#FFF",
+        200: "#F4F4F4",
         
-        'smd' : '800px',
-           // => @media (min-width: 860px) { ... }
-  
-        'mmd': '860px',
-          // => @media (min-width: 860px) { ... }
-  
-        'xmd' : '930px',
-          // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-  
-         'xlg' : '1110px',
-             // => @media (min-width: 1110px) { ... }
-  
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        'xxl': '1390px',
-        // => @media (min-width: 1280px) { ... }
-  
-        
-      },
+      },
+
+      richblack: {
+        100: "#B7B7B7",
+        200: "#7A7A7A",
+        300: "#515050",
+        400: "#54595F",
+        500: "#0C0000",
+
+      },
+     
+      "dark-green": {
+        100: "#23A455",
+        200: "#007936",
+       
+      
+      },
+      "light-green":{
+        100: "#6EC1E4",
+        200: "#B7DA7A",
+        300: "#BAD900",
+        
+
+      },
+      "yellow":{
+        100: "#E8C204",
+        200: "#EEA90E",
+
+      },
+     
+
+    },
+
+    screens : {
+
+      'dxs' : '300px',
+      // => @media (min-width: 300px) { ... }
+
+      'xxs' : '400px',
+      // => @media (min-width: 400px) { ... }
+
+      'xsm' : '458px',
+      // => @media (min-width: 400px) { ... }
+
+      'xs' : '500px',
+      // => @media (min-width: 500px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      
+      'smd' : '800px',
+         // => @media (min-width: 860px) { ... }
+
+      'mmd': '860px',
+        // => @media (min-width: 860px) { ... }
+
+      'xmd' : '930px',
+        // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+
+       'xlg' : '1110px',
+           // => @media (min-width: 1110px) { ... }
+
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'xxl': '1390px',
+      // => @media (min-width: 1280px) { ... }
+
+      
+    },
+
+    extend: {
+      
     },
   },
   plugins: [],
-}
+};
 
