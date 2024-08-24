67c7c3f152132a60b11b0a446c6391a3e73430a8
Ashique <ansariazad7864@gmail.com>
2024-08-24 23:07:38 +0530

    footer data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import HeroSection from './components/Home/HeroSection'
 import './App.css'
 import Product from './components/Home/Product'
+import Footer from './components/Home/Footer'
 // import OurVision from './components/Home/OurVision'
 // import {protectionData} from './data/home/protectionData'
 
@@ -10,7 +11,7 @@ const App = () => {
         <HeroSection/>
            <HeroSection/>
            <Product />
-          
+          <Footer/>
         </div>
         
     )
---
+++ src/data/home/footer-data.js
@@ -0,0 +1,104 @@
+import logos from '../../assets/logo.png'
+
+export const footerData = {
+    description: {
+        img: logos,
+        details:
+            'The Company Oswal Crop Protection Pvt. Ltd. has been Established by Mr. Ramesh Bansal is an ISO 9001:2015 certified company and is recognised as one of the leading manufacturers and suppliers of Insecticides, Herbicides, Fungicides, Weedicides and Plant Growth Regulators. '
+    },
+    sitesAndLocations: {
+        title: 'Sites & Locations :',
+        content:
+            'Company presence in the States of Andhra Pradesh, Chattisgarh, Haryana, Jammu & Kashmir, Karnataka, Madhya Pradesh, Maharashtra, Punjab, Rajasthan, Tamilnadu, Telengana, Uttar Pradesh, Uttrakhand.'
+    },
+    contactUs: {
+        title: 'Contact Us',
+        address:
+            '310, Gupta Tower, Commercial Complex, Azadpur, Delhi - 110033 (India)',
+        phone: 'Phone : +91-11-49072043',
+        email: 'Email : info@oswalcrop.com'
+    },
+    works: {
+        title: 'Works',
+        address:
+            'Khasra No. 55/2/1, Pipli Khera Road, Village Ram Nagar, Tehsil Gannaur, Distt. Sonipat, Haryana - 131001'
+    },
+    navigation1: [
+        {
+            id: 1,
+            label: 'Home',
+            url: '/'
+        },
+        {
+            id: 2,
+            label: 'Photo Gallery',
+            url: '/'
+        },
+        {
+            id: 3,
+            label: 'Career',
+            url: '/'
+        },
+        {
+            id: 4,
+            label: 'Vacancy',
+            url: '/'
+        },
+        {
+            id: 5,
+            label: 'Contact',
+            url: '/'
+        },
+        {
+            id: 6,
+            label: 'Terms & Condition',
+            url: '/'
+        },
+        {
+            id: 7,
+            label: 'FAQs',
+            url: '/faqs'
+        }
+    ],
+    navigation2: [
+        {
+            id: 1,
+            label: 'Insecticides',
+            url: '/'
+        },
+
+        {
+            id: 2,
+            label: 'Fungicides',
+            url: '/'
+        },
+        {
+            id: 3,
+            label: 'Herbicides',
+            url: '/'
+        },
+
+        {
+            id: 4,
+            label: 'Fertilizer',
+            url: '/'
+        },
+        {
+            id: 5,
+            label: 'Bio Products',
+            url: '/'
+        },
+        {
+            id: 6,
+            label: 'Micro Nutrients',
+            url: '/'
+        },
+        {
+            id: 7,
+            label: 'Plant Growth Regulators',
+            url: '/'
+        }
+    ]
+}
+
+
