6904205a5e8311c95585f346696d30ec84c1029a
Ashique <ansariazad7864@gmail.com>
2024-08-24 21:17:00 +0530

    completed our Products

--- src/components/Home/OurVision.jsx
+++ src/components/Home/OurVision.jsx
@@ -1,112 +1,100 @@
-import React from 'react'
+// import React from 'react'
 import India from '../../images/Home/india_icon.png'
 import Export from '../../images/Home/export_icon.png'
 import Vision from '../../images/Home/vision_mission.png'
 
 const OurVision = () => {
   return (
-    <section className='w-full py-20 flex flex-col  gap-y-12 '>
-
-
+    <section className='flex flex-col w-full py-20 gap-y-12 '>
       {/* --------- heading part ---------- */}
 
-      <div className=' w-10/12 mx-auto flex flex-col gap-y-7  items-center'>
-
-        <h1 className='text-[40px] font-bold text-dark-green-200'>Vision & Mission</h1>
+      <div className='flex flex-col items-center w-10/12 mx-auto gap-y-7'>
+        <h1 className='text-[40px] font-bold text-dark-green-200'>
+          Vision & Mission
+        </h1>
 
         {/* ------- small line ------ */}
 
-        <div className='h-1 w-24 bg-richblack-500'></div>
-
+        <div className='w-24 h-1 bg-richblack-500'></div>
       </div>
 
       {/* ------------- content part ------------ */}
 
       <div className='map-bg'>
-
         {/* --------- container ----------- */}
 
-        <div className='w-10/12 mx-auto  flex justify-between gap-x-10'>
-
+        <div className='flex justify-between w-10/12 mx-auto gap-x-10'>
           {/* --------- left side  ------- */}
 
           <div className='w-[45%] border-richblack-300 flex flex-col gap-y-7'>
-
             {/* ------- vision ----- */}
 
             <div className='flex flex-col gap-y-4 '>
-
-              <h1 className='text-2xl text-dark-green-200 font-bold '>Our Vision</h1>
-              <p className='w-[85%]'>We are determined to be one of the companies
-                working for the up-liftment of farmers. </p>
-
+              <h1 className='text-2xl font-bold text-dark-green-200 '>
+                Our Vision
+              </h1>
+              <p className='w-[85%]'>
+                We are determined to be one of the companies working for the
+                up-liftment of farmers.{' '}
+              </p>
             </div>
 
             {/* ------- misison ----- */}
 
             <div className='flex flex-col gap-y-4 '>
-
-              <h1 className='text-2xl text-dark-green-200 font-bold '>Our Mission</h1>
-              <p className='w-[85%]'>To be a part of pesticides industry for complimenting
-                sustainable agriculture in the country.</p>
-
+              <h1 className='text-2xl font-bold text-dark-green-200 '>
+                Our Mission
+              </h1>
+              <p className='w-[85%]'>
+                To be a part of pesticides industry for complimenting
+                sustainable agriculture in the country.
+              </p>
             </div>
 
             {/* --------------- icons ----------------- */}
 
             <div className='w-[80%] flex gap-x-20 justify-center'>
-
               {/* -------- india -------- */}
 
               <div className='flex flex-col items-center gap-y-4'>
-
-
                 <div className='border-[6px] border-richblack-50 w-[110px] h-[110px] rounded-full'>
-                  <img src={India} alt="india" className='w-full h-full object-cover' />
+                  <img
+                    src={India}
+                    alt='india'
+                    className='object-cover w-full h-full'
+                  />
                 </div>
 
                 <p className='text-lg font-bold'>All India Presence </p>
               </div>
 
-
-
               {/* -------- Exports -------- */}
 
               <div className='flex flex-col items-center gap-y-4'>
-
-
                 <div className='border-[6px] border-richblack-50 w-[110px] h-[110px] rounded-full'>
-                  <img src={Export} alt="export" className='w-full h-full object-cover' />
+                  <img
+                    src={Export}
+                    alt='export'
+                    className='object-cover w-full h-full'
+                  />
                 </div>
 
                 <p className='text-lg font-bold'>Exports of Pesticides</p>
               </div>
-
             </div>
-
-
-
-
-
           </div>
-
-
 
           {/* --------- right side ----------- */}
 
           <div className='w-[45%] '>
-
-            <img src={Vision} alt="vision" className='w-full h-full object-cover rounded-2xl' />
-
+            <img
+              src={Vision}
+              alt='vision'
+              className='object-cover w-full h-full rounded-2xl'
+            />
           </div>
-
         </div>
-
       </div>
-
-
-
-
     </section>
   )
 }
---
+++ src/components/common/Card.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { ImPointRight } from 'react-icons/im';
+
+const Card = ({ title, picture, buttonText, showIcon, className }) => {
+  return (
+    // ---------------Card Designing-------------------
+    <div className={`relative ${className}`}>
+      <div className='h-full shadow-lg bg-gradient-to-r from-darkGray via-lightGray to-darkGray rounded-3xl'>
+        <div className='flex flex-col items-center gap-10 p-5'>
+          <h1 className='text-[1.8rem] font-bold text-dark-green-200'>{title}</h1>
+          <div className='flex justify-center w-full'>
+            <img src={picture} alt='' className='object-cover w-full h-full' />
+          </div>
+          <button className='flex px-4 py-2 text-white transition-transform duration-300 rounded-lg bg-light-green-200 hover:animate-float hover:shadow-lg hover:bg-richblack-500 hover:text-white-shade-100'>
+            {buttonText}
+            {showIcon && (
+              <span className='p-1'>
+                <ImPointRight />
+              </span>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+// ---------------Defining Props-------------------
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  buttonText: PropTypes.string.isRequired,
+  picture: PropTypes.string,
+  showIcon: PropTypes.bool,
+  className: PropTypes.string, 
+};
+//----------------Defining Default props-----------------
+Card.defaultProps = {
+  showIcon: true,
+  className: '', 
+};
+
+export default Card;
