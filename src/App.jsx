e86309ff3ff59b4e9298c018ebccc52b3eb07cfd
Mushafiya <kmushafiya003@gmail.com>
2024-08-24 11:19:38 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
-import HeroSection from  './components/home/HeroSection'
+import HeroSection from './components/home/HeroSection'
 // import Protection from './components/home/Protection'
 import './App.css'
+// import OurVision from './components/Home/OurVision'
 // import {protectionData} from './data/home/protectionData'
 
-const App=()=> {
- return(
-  <div >
-  <HeroSection />
-   
-  </div>
- )
-  
+const App = () => {
+    return (
+        <div className='w-screen min-h-screen font-open-sans overflow-x-hidden'>
+            <HeroSection />
+            {/* <OurVision /> */}
+
+        </div>
+    )
+
 }
 
 export default App
