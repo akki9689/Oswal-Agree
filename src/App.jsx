c0c5fb9935f441e8de8c5e0c0de0fc27b4aa4a63
Mushafiya <kmushafiya003@gmail.com>
2024-08-24 20:14:35 +0530

    Vision and Mission Section Completed with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import HeroSection from './components/home/HeroSection'
 // import Protection from './components/home/Protection'
 import './App.css'
-import OurVision from './components/Home/OurVision'
+// import OurVision from './components/Home/OurVision'
 // import {protectionData} from './data/home/protectionData'
 
 const App = () => {
     return (
         <div className='w-screen min-h-screen font-open-sans overflow-x-hidden'>
             <HeroSection />
-            <OurVision />
+            {/* <OurVision /> */}
 
         </div>
     )
--- src/components/Home/OurVision.jsx
+++ src/components/Home/OurVision.jsx
@@ -5,14 +5,14 @@ import Vision from '../../images/Home/vision_mission.png'
 
 const OurVision = () => {
   return (
-    <section className='w-full py-20 flex flex-col  gap-y-12 '>
+    <section className='w-full py-20 flex flex-col  gap-y-10'>
 
 
       {/* --------- heading part ---------- */}
 
       <div className=' w-10/12 mx-auto flex flex-col gap-y-7  items-center'>
 
-        <h1 className='text-[40px] font-bold text-dark-green-200'>Vision & Mission</h1>
+        <h1 className='xs:text-[40px] text-[28px] font-bold text-dark-green-200'>Vision & Mission</h1>
 
         {/* ------- small line ------ */}
 
@@ -22,64 +22,64 @@ const OurVision = () => {
 
       {/* ------------- content part ------------ */}
 
-      <div className='map-bg'>
+      <div className='map-bg '>
 
         {/* --------- container ----------- */}
 
-        <div className='w-10/12 mx-auto  flex justify-between gap-x-10'>
+        <div className='w-10/12 mx-auto  flex mmd:flex-row flex-col gap-y-16 justify-between gap-x-4 '>
 
           {/* --------- left side  ------- */}
 
-          <div className='w-[45%] border-richblack-300 flex flex-col gap-y-7'>
+          <div className='xlg:w-[45%] mmd:w-[48%] flex flex-col gap-y-7'>
 
             {/* ------- vision ----- */}
 
-            <div className='flex flex-col gap-y-4 '>
+            <div className='flex flex-col items-center mmd:items-start gap-y-4 '>
 
               <h1 className='text-2xl text-dark-green-200 font-bold '>Our Vision</h1>
-              <p className='w-[85%]'>We are determined to be one of the companies
+              <p className='xxl:w-[85%] mmd:w-full xs:w-[85%] w-[95%] pr-2 xxl:pr-0 mmd:text-left text-center'>We are determined to be one of the companies
                 working for the up-liftment of farmers. </p>
 
             </div>
 
             {/* ------- misison ----- */}
 
-            <div className='flex flex-col gap-y-4 '>
+            <div className='flex flex-col items-center mmd:items-start  gap-y-4 '>
 
               <h1 className='text-2xl text-dark-green-200 font-bold '>Our Mission</h1>
-              <p className='w-[85%]'>To be a part of pesticides industry for complimenting
+              <p className='xxl:w-[85%] mmd:w-full xs:w-[85%] w-[95%]  pr-2 xxl:pr-0 mmd:text-left text-center'>To be a part of pesticides industry for complimenting
                 sustainable agriculture in the country.</p>
 
             </div>
 
             {/* --------------- icons ----------------- */}
 
-            <div className='w-[80%] flex gap-x-20 justify-center'>
+            <div className='lg:w-[80%] flex  lg:gap-x-20 mmd:gap-x-10 mmd:justify-center xs:justify-evenly justify-between gap-x-4 xsm:gap-x-0'>
 
               {/* -------- india -------- */}
 
-              <div className='flex flex-col items-center gap-y-4'>
+              <div className='flex flex-col items-center gap-y-4 '>
 
 
-                <div className='border-[6px] border-richblack-50 w-[110px] h-[110px] rounded-full'>
+                <div className='border-[6px] border-richblack-50 mmd:w-[110px] xs:w-[120px]  mmd:h-[110px] xs:h-[120px] w-[110px] h-[110px] rounded-full transition-all duration-700 hover:scale-90 hover:opacity-70'>
                   <img src={India} alt="india" className='w-full h-full object-cover' />
                 </div>
 
-                <p className='text-lg font-bold'>All India Presence </p>
+                <p className='xmd:text-lg mmd:text-[16px] xs:text-lg font-bold text-center w-[80%]'>All India Presence </p>
               </div>
 
 
 
               {/* -------- Exports -------- */}
 
-              <div className='flex flex-col items-center gap-y-4'>
+              <div className='flex flex-col items-center gap-y-4 '>
 
 
-                <div className='border-[6px] border-richblack-50 w-[110px] h-[110px] rounded-full'>
+                <div className='border-[6px] border-richblack-50 mmd:w-[110px] xs:w-[120px]  mmd:h-[110px] xs:h-[120px] w-[110px] h-[110px] rounded-full transition-all duration-700 hover:scale-90 hover:opacity-70'>
                   <img src={Export} alt="export" className='w-full h-full object-cover' />
                 </div>
 
-                <p className='text-lg font-bold'>Exports of Pesticides</p>
+                <p className='xmd:text-lg mmd:text-[16px] xs:text-lg font-bold text-center w-[80%]'>Exports of Pesticides</p>
               </div>
 
             </div>
@@ -94,7 +94,7 @@ const OurVision = () => {
 
           {/* --------- right side ----------- */}
 
-          <div className='w-[45%] '>
+          <div className='xlg:w-[45%] mmd:w-[48%] w-full '>
 
             <img src={Vision} alt="vision" className='w-full h-full object-cover rounded-2xl' />
 
