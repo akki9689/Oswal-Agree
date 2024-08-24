9468e766732d83811ab62ccab8689b5f9197fbd0
pankaj-03 <pankajsunal045@gmail.com>
2024-08-25 01:06:32 +0530

    Desktop Navbar Component starting

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import HeroSection from  './components/home/HeroSection'
 // import Protection from './components/home/Protection'
 import './App.css'
 // import {protectionData} from './data/home/protectionData'
+import Navbar from './components/Home/Navbar'
 
 const App=()=> {
  return(
   <div >
-  <HeroSection />
+  {/* <HeroSection /> */}
+  <Navbar/>
    
   </div>
  )
