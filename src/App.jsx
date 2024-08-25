46095e6f7282598223a20bfa325bebb5cb3d2946
akki9689 <akashpuri259@gmail.com>
2024-08-25 17:00:35 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import HeroSection from './components/Home/HeroSection'
 import './App.css'
 import Product from './components/Home/Product'
 import Footer from './components/Home/Footer'
-// import Companyprotection from './components/home/Companyprotection'
+import Companyprotection from './components/home/Companyprotection'
 // import OurVision from './components/Home/OurVision'
-// import {protectionData} from './data/home/protectionData'
+import {protectionData} from './data/home/protectionData'
 import Navbar from './components/Home/Navbar'
 
 const App = () => {
     return (
         <div className='w-screen min-h-screen overflow-x-hidden font-open-sans'>
         <HeroSection/>
-           
+           <Companyprotection data={protectionData}/>
            <Product />
           <Footer/>
         </div>
