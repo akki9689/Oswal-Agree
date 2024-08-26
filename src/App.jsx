aacc260d599db5e953dd34c53cc28d613375ad1b
akki9689 <akashpuri259@gmail.com>
2024-08-27 01:44:53 +0530

    vision2 created

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ const App = () => {
     return (
         <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-open-sans'>
            <Navbar/>
-           <HeroSection/>
+           {/* <HeroSection/>
            <Companyprotection/>
            <Product />
            <OurVision/>
 
            <IconSection/>
-           <PhotoGallery/>
-           
+           <PhotoGallery/> */}
+           <Companyprofile/>
             <Footer/>
           
         </div>
