53b7eedfe1d8358e4499ba59433098c49770f623
pankaj-03 <pankajsunal045@gmail.com>
2024-08-26 17:09:47 +0530

    ..

---
+++ src/data/nav-links.js
@@ -0,0 +1,24 @@
+export const navbarData = {
+
+    navLinks : [
+         {
+            id:1,
+            title: 'Home',
+            path: '/'
+         },
+
+         {
+            id:2,
+            title: 'About Us',
+            dropdown:[
+                {id:21 , title:'Company Profile' , path:'/about-us/company-profile'},
+                {id:22 , title: 'Vision and Mission' , path: '/about-us/vision-and-mission'},
+                {id:23 , title:'Awards and Recognition' , path:'/about-us/awards-and-recognition'},
+                {id:24 , title:'Terms and Condition' , path:'/about-us/terms-and-condition'}
+            
+            ]
+         }
+
+        
+    ]
+}
