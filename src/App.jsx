e77a322340a87ebc17cfc285612014b30a729fb3
Mushafiya <kmushafiya003@gmail.com>
2024-08-26 11:05:02 +0530

    Added Reusable Heading Component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import OurVision from './components/Home/OurVision'
 import IconSection from './components/Home/IconSection'
 
 
+
 const App = () => {
     return (
         <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-open-sans'>
