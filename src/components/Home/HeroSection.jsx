4b47b66c81273388daed7b30c8229560dcafc40b
nalini jadhav <nalini@edulab.in>
2024-08-26 13:53:39 +0530

    herosection

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -13,38 +13,64 @@ import image3 from '../../images/Home/herosection/agro3.jpg';
 import image4 from '../../images/Home/herosection/agro4.jpg';
 import image5 from '../../images/Home/herosection/agro5.jpg';
 
-const images = [image1, image2, image3, image4, image5];
+const slides = [
+  { 
+    image: image1, 
+    title: "One of the Leading Manufacturers of Agrochemicals in India", 
+  },
+  { 
+    image: image2, 
+    title: "Support for Sustainable Agriculture", 
+  },
+  { 
+    image: image3, 
+    title: "Har Kadam Par Kisaano Ka Humsafar", 
+  },
+  { 
+    image: image4, 
+    title: "Wide Range of Insecticides, Fungicides, Herbicide & PGRs", 
+  },
+  { 
+    image: image5, 
+    title: "Highest Quality Products & Solutions", 
+  }
+];
 
 const HeroSection = () => {
   return (
     <section id="home" className="relative w-full">
       <div className="p-0 container-fluid">
         <div className="row">
-          <div className="col ">
+          <div className="col">
             <Swiper
-              spaceBetween={30} // Adjusted space between slides for responsiveness
+              spaceBetween={30}
               slidesPerView={1}
               navigation
               pagination={{ clickable: true }}
               modules={[Navigation, Pagination, Autoplay, EffectCube]}
-              className="h-[70vh] md:h-[80vh] lg:h-[90vh] xl:h-[100vh] overflow-hidden rounded-lg"
+              className="h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh] overflow-hidden rounded-lg"
               loop={true}
               autoplay={{ delay: 10000 }}
               effect="cube"
               cubeEffect={{
                 shadow: true,
                 slideShadows: true,
-                shadowOffset: 30,
+                shadowOffset: 40,
                 shadowScale: 0.94,
               }}
             >
-              {images.map((image, index) => (
-                <SwiperSlide key={index} className=''>
-                  <img 
-                    src={image} 
-                    alt={`Slide ${index}`} 
-                    className="object-cover w-full h-full" 
-                  />
+              {slides.map((slide, index) => (
+                <SwiperSlide key={index} className="">
+                  <div className="relative w-full h-full">
+                    <img 
+                      src={slide.image} 
+                      alt={`Slide ${index}`} 
+                      className="object-cover w-full h-full" 
+                    />
+                    <div className="absolute bottom-8 left-4 bg-gray-600 bg-opacity-80 text-white p-2 md:bottom-12 md:left-10 md:p-3">
+                      <h2 className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-regular">{slide.title}</h2>
+                    </div>
+                  </div>
                 </SwiperSlide>
               ))}
             </Swiper>
