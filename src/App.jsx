b7e98c8ceef9f223d7453cba147ecf35900ea91c
akki9689 <akashpuri259@gmail.com>
2024-08-27 21:32:01 +0530

    ksk

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,12 @@ import Companyprofile from './components/about/Companyprofile'
 import PhotoGallery from './components/Galllery/PhotoGallery'
 import Visionabt from './components/about/Visionabt'
 import Awards from './components/about/Awards'
+import Popupname from './components/common/Popupname'
 const App = () => {
     return (
         <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-open-sans'>
            <Navbar/>
+           
            {/* <HeroSection/>
            <Companyprotection/>
            <Product />
