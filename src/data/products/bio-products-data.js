6a9ee9497f0c0ba60476a9b1bf397029edd7a8b5
nalini jadhav <nalini@edulab.in>
2024-08-27 13:07:32 +0530

    bioproduct data

---
+++ src/data/products/bio-products-data.js
@@ -0,0 +1,265 @@
+
+
+
+
+
+BioproductName: [
+    {
+        id: 1,
+        name: "Thunder",
+        img: Thunder,
+        activeIngredient: "Liquid Extract Organic Pest Controller",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton, Cabbage, Brinjal, Sugarcane, Paddy, Pea, Vegetables"],
+            dose: "It should be diluted at the rate of 1-2 ml. in 1 litre of water and generally applied at the rate of 150 litre of emulsion per acre.",
+            pests: ["Leaf eating caterpillar"],
+            uses: "It is biologically derived Extract Liquid Organic Pest Controller for foliar application. It contains Nitrogeneous components. which controls the life cycle of the pests.",
+            advantages: [
+                "It is recommended for the control of common pests, insects on so may crops on all suckings and chewing types of pest. It works biologically to control its life cycle by physiological effects."
+            ],
+            application: "Plants Should be uniformly drenched.",
+        },
+
+    },
+    {
+        id: 2,
+        name: "Borohit Plus",
+        img: Borohit ,
+        activeIngredient: "All Vegetables & Cereal and Fruit Crops",
+        url: "#",
+        details: {
+            targetCrops: ["Potato", "Vegetables", "Fruits"],
+            dose: "250-300 gm/acre in 125 -150 liter water",
+            pests: ["............."],
+            uses: "Borohit Plus is a combination of Boron (Ca as CaO 30% and B 4%) in chelated form which is important for physiological and biochemical processes in the plant. Transport of boron and calcium to the vegetable decreases during fruit development. Boron improves the solubility and mobility of calcium in vegetables and regulates the carbohydrate metabolism, besides helping in the absorption of Nitrogen.",
+            advantages: [
+                "Thus Borohit Plus help in the free transport of calcium coupled with boron and performs even in calcareous soil and hick alkaline soil since it is a chelated mix. Boron is essential to maintain the structural integrity of plant membranes along with calcium and is involved in nucleic acid metabolism. Production of vegetable crops is adversely affected much more than vegetative growth with a low supply of available boron."
+            ],
+            application: "Make a solution of Borohit Plus 250-300 gm per acre in 125-150 liter water. Use in 2nd spray 7-10 day Interval. Apply In a basal dose of 500 gm per acre with mixed fertilizer.",
+
+        },
+
+    },
+    {
+        id: 3,
+        name: "Dhoom",
+        img: Dhoom,
+        activeIngredient: "Organic Based Primary Elements",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Vegetables", "Fruits", "All Crops", "Cereals", "Pulses", "Oil Seeds"],
+            dose: "1 Kg to 2 Kg per Acre",
+            pests: [".........."],
+            uses: "It is granulated organic manure derived from natural sources. it is used for increasing the yield and quality of all crops, including cereals, cotton, oil seeds, pulses, vegetables, fruit crops, plantation crops, etc.",
+            advantages: [
+                "Increases yield significantly, stops flower and fruit drop, improves the quality of crops, enhances protein synthesis, and increases tolerance against diseases and climatic stress. Compatible with all types of fertilizers and pesticides."
+            ],
+            application: "It can be applied by way of broadcast treatment as a standalone product or along with other fertilizers as a basal dose / top dressing. Multiple applications at critical stages of crop ensure best results."
+        }
+    },
+    {
+        id: 4,
+        name: "Diamond Gold",
+        img: Diamond ,
+        activeIngredient: "Crop Vigour Stimulant",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Mango", "Sugarcane", "Paddy", "Maize", "Wheat", "Bajra", "Mustard", "Soybean", "Tobacco", "Vegetables", "Fruits", "Pulses", "Jawar"],
+            dose: "4 Kg/Acre",
+            pests: ["..........."],
+            uses: "Paddy, Sugarcane, Wheat, Pulses, Cotton, Tobacco, Maize, Jawar, Bajra, Soybean. Mango, Mustard, Fruit, and vegetables are in all the sectors of agriculture, horticulture, floriculture, pasture land, etc. It gives excellent results for all Root-type crops such as Groundnut, Potato, Onion, Ginger, and Garlic. It is recommended to be mixed with fertilizers. It can also be used as a broadcast treatment, spot, or band application. ",
+            advantages: [
+                "Accelerating Germination & improving greenery in plants. Enhancing Seedling Vigour & Root Growth. Improving Uptake & Translocation of Macro & Micro Nutrients. Inducing early maturity and inducing more Flowering forming substances & increasing more Flowers. Helping Plants to resist drought Resulting in Higher Yields & Better Quality of Crops Produce."
+            ],
+            application: "......................"
+        }
+    },
+    {
+        id: 5,
+        name: "Diamond Gold",
+        img: Diamond,
+        activeIngredient: "Organic Fertilizer (Liquid)",
+        url: "#",
+        details: {
+            targetCrops: ["Paddy", "Fruits", "All Crops", "Horticulture"],
+            dose: "250-500 ml/acre",
+            pests: ["........"],
+            uses: "Diamond Gold is a distinguished bio-organic fertilizer generated by amino acids and other micronutrients. It affects the biochemical activities and supports and accelerates the process of building up of natural phytohormones (auxins, gibberellins, cytokinins, ethylene) within the plant, plus, phenomenally enhances its immunity potential.",
+            advantages: [
+                "Effectively secures the plant from natural disparities resulting in the clean splendor, magnificent tillering, and might of retention of flowers and fruits. It also promotes the extensive growth of roots for the overall development of the plant. It also adds to the quality of yield and produce."
+            ],
+            application: "Application: 1. For Paddy, at planting / 10-12 days after planting. Second application – 30-35 days after plantation. 2. For Fruits & Horticulture crops, repeat in 15-20 days time. Diamond Gold is a Unique Plant Care Product that helps to: 1. Boost Flowering. 2. Increase Fruit Size and Quality. 3. Increase Grain Size and Grain Formation. 4. Increase Yield. The direction of use: 250-500 ml per acre."
+        }
+    },
+    {
+        id: 6,
+        name: "Gazab-G",
+        img: Gazab-G,
+        activeIngredient: "Seaweed 7% + Protein Hydrolysate 3%",
+        url: "#",
+        details: {
+            targetCrops: ["Chilli", "Sugarcane", "Paddy", "Wheat", "Vegetables", "Fruits", "Oil Seeds"],
+            dose: "2 Kg/Acre",
+            pests: ["......................"],
+            uses: "It is a granular yield booster derived from natural resources. Absorbed through the roots, it enhances the sun rays absorption capacity (Photosynthesis) of crops. Which gives amazing increase in their productivity and quality.",
+            advantages: [
+                "Wheat, Paddy, Sugarcane, Chillies, Oilseeds, Fruits and vegetables etc."
+            ],
+            application: "Recommended dosage for it is 2 Kg per acre."
+        }
+    },
+    {
+        id: 7,
+        name: "Grow UP",
+        img: Grow,
+        activeIngredient: "Bloom Fresh Active Constituent Cytokinins & Enzymes ",
+        url: "#",
+        details: {
+            targetCrops: ["Vegetables", "Fruits"],
+            dose: "250 ml/acre in 200 liter water",
+            pests: ["......."],
+            uses: "Use: To increase fruit size and yield in fruits & vegetables. Antidote: Gastric lavage with warm water. In case of poisoning, call a physician immediately.",
+            advantages: [
+                "With an objective to fulfill the demands of our clients, we are engaged in providing a wide array of Active Constituent Cytokinins And Enzymes."
+            ],
+            application: "............"
+        }
+    },
+    {
+        id: 8,
+        name: "Osovita Gold",
+        img: Osovita ,
+        activeIngredient: "Liquid Sea-Weed Based Bio-Extract",
+        url: "#",
+        details: {
+            targetCrops: ["Sugarcane", "Paddy", "Tea", "Potato", "Banana", "Wheat", "Soybean", "Vegetables"],
+            dose: "3 ml/acre in 1 liter water",
+            pests: ["........"],
+            uses: "It is Organic Product derived from natural sources of vegetable sea weed. It is used to increase the yield and quality of produce in paddy, wheat, sugarcane, vegetable, soyabean, potato, banana and tea. It is compatible with chemical fertilizer and pesticides. Non toxic and safe to use.",
+            advantages: [
+                "........... "
+            ],
+            application: "........."
+        }
+    },
+   {
+        id: 9,
+        name: "Osovita-G",
+        img: Osovita ,
+        activeIngredient: "Granulated Sea Weed Based Bio-Extract",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Grapes", "Tomato", "Paddy", "Citrus", "Banana", "Soybean", "Pulses"],
+            dose: "1 to 2 ml. per litre of water",
+            pests: ["........"],
+            uses: "Humic Acid and Amino Acid 14% w/w, Solid Contents (Bentonite Granules) Q.S. to make, Total 100% w/w. Improved soil structure by making the soil more porous, permeable and aerated. Improved germination of seeds, root development chlorophyill and vitamin content. lmproved uptake and translocation of micro/macro nutrients chlorophyll and vitamin content. Improved uptake and translocation of micro/macro nutrients chlorophyll and vitamin content. Improved uptake and translocation of micro / macro nutrients & stabilizes pH. lnducing early maturity by enhancing enzyme activity. Encourages the development of beneficial soil micro organisms. Helping plants to resist drought. Resulting in higher yields and better quality or crops produce.",
+            advantages: [
+                "lt is recommended on various crops namely Soybean, Cotton, Paddy, Pulses, Citrus, Banana, Grapes, Tomatoes and other vegetables and fruits."
+            ],
+            application: "It is recommended to be mixed with fertilizers. It can also be used alone as broadcast treatment, spot or band application."
+        }
+    },
+    {
+        id: 10,
+        name: "Oswal Gold",
+        img: Oswal ,
+        activeIngredient: "Humic-Humin-Fulvic-Humate 95%",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Grapes", "Cabbage", "Brinjal", "Mango", "Sugarcane", "Tomato", "Paddy", "Tea", "Maize", "Potato", "Banana", "Wheat", "Bajra", "Mustard", "Soybean", "Apple", "Papaya", "Tobacco", "Vegetables", "Mentha", "Jawar", "Pumpkin"],
+            dose: "Use in drip irrigation at the rate of 500 grams per acre.",
+            pests: ["........"],
+            uses: "Use in crops Paddy, Wheat, Maize, Mustard, Jowar, Bajra, Potato, Sugarcane, Cotton, Tobacco, Soybean, Tomato, Brinjal, Cabbage and Pumpkin Vegetables, Grapes, Mango, Banana, Apple, Papaya, Tea, Mentha and Flowers crops.",
+            advantages: [
+                "Treat large size seeds (Paddy, Wheat, Maize, Mustard etc.) at the rate of 5 grams per kg and small size seeds (Brinjal, Chilli, Tomato, Cabbage etc.) at the rate of 5 grams per 50 grams seed.",
+            ],
+            application: "Spray at the rate of 2-3 grams per liter of water."
+        }
+    },
+    {
+        id: 11,
+        name: "Oswal Punch",
+        img: Oswal ,
+        activeIngredient: "A Bio Extract Pest Controller",
+        url: "#",
+        details: {
+            targetCrops: ["Rice", "Sugarcane"],
+            dose: "4 Kg/acre",
+            pests: ["Early shoot borer", "Leaf folder", "Stem borer", "Top borer"],
+            uses: "It is very useful for diseases like twisting, tying, Mit-Magic (red zoom), aphids, jasids etc. Provides strength to fight disease. Quantity : 100 ml Spray Mit-Magic in 150 to 200 liters of water.",
+            advantages: [
+                "Oswal Punch based on latest biotechnology plants is essential for plant essential hydrolyzed digestive protein, carbohydrates, seaweed extracts and concentrates in microscopic amounts; the use of granular Oswal Punch rich in nutritional nutrients provides excellent quality of crops. It is necessary to have sufficient moisture in the field at the time of use.",
+            ],
+            application: "4 kg per acre on crops Oswal Punch should be used before sowing or at the time of sowing or in standing dressing on stand crop by mixing it with other fertilizers."
+        }
+    },
+    {
+        id: 12,
+        name: "Oswal Zinc",
+        img: Oswal ,
+        activeIngredient: "Microbial Zinc Solubilizer",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Grapes", "Sugarcane", "Paddy", "Maize", "Wheat", "Vegetables", "Fruits", "Pomgranate", "Cereals", "Pulses", "Oil Seeds", "Flowers", "Spices", "Orange"],
+            dose: "2-4 Kg/Acre",
+            pests: ["........"],
+            uses: "It is a micronutrient of macro importance and it acts as a metal activator of enzymes. Its deficiency causes considerable yield losses and plants become prone to diseases. Initial symptoms start with intervienal chiorosis of younger leaves leading to a reduction in shoot growth and shortening of internodes. Mottle leaf, little leaf, etc. in the case of trees are symptoms of zinc deficiency. This is a common problem in alkaline and calcareous soils.",
+            advantages: [
+                "It is very useful for all crops like – cereals (wheat, paddy, maize), pulses, oilseeds, fruits (grapes, oranges, pomegranates), vegetables, spices, flowers, cotton, sugarcane etc.",
+            ],
+            application: "Thoroughly along with other bio-fertilizers and bio- pesticides (to be applied in soil) with fully decomposed organic manure (OM) or soil (10 kg per acre) from the same field. Broadcasting the mixture uniform at the time of final land preparation or immediately before / after sowing / transplanting or up to 45 days of sowing / transplanting flower by light irrigation. In case of orchards, apply 50-100 g is mixed with 2-4 kg (OM) per tree twice a year."
+        }
+    },
+    {
+        id: 13,
+        name: "SAT-VAT",
+        img: SAT-VAT ,
+        activeIngredient: "Superspreader",
+        url: "#",
+        details: {
+            targetCrops: ["All Crops"],
+            dose: "25 ml/100 Liter of water.",
+            pests: ["........"],
+            uses: "It can be used with all category agrochemicals that is insecticide, Fungicide, Herbicide, Plant Growth Regulator, Fertilizer & Micro Nutrient.",
+            advantages: [
+                "A non-ionic spray adjuvant, it is a Superspreader surfactant on a Trisiloxane Ethoxylate. Promotes spray volume reduction thereby improving spray coverage. Promotes rapid uptake of agrochemicals into plants via stomatal infiltration making the spray solution rainfast. It is most effective as a tank side adjuvant when spray mixture are within the pH range of 5-8 and are use 24 hours within preparation of the spray solution. Use rates depend on crop, stage, agrochemical & spray volume requirements.",
+            ],
+            application: "Strictly follow agrochemicals, PGR’s and fertilizers label instructions for lank filling, fill water volume to only 90%. Add it is mix thoroughly and add the remaining water."
+        }
+    },
+    {
+        id: 14,
+        name: "MP Super",
+        img: MP-Super ,
+        activeIngredient: "Multipurpose Bio Plant Protector Eco Friendly",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Red Gram", "Brinjal", "Okra", "Chilli", "Tomato", "Soybean", "Turmeric", "Vegetables", "Lentil", "Mint"],
+            dose: "2 ml/acre in 1 liter water",
+            pests: ["White Fly", "BPH (Brown Plant Hopper)", "White fly Brown plant hopper", "Leaf eating caterpillar"],
+            uses: "It is an Environmental Modesty Natural Bio Product. This is made especially for organic farming to protect many pests (insects) for all vegetable crops. This provides the power to fight the major pests in the crops (such as all kinds of monarchs, caterpillars, white fly, white and black plant Hooper, and all the insects that come in all kinds of vegetables) And develops plants by creating new healthy leaves and growth.",
+            advantages: [
+                "Keeps the plants healthy and their immunity also increases, due to which the growth of the plant is rapid, resulting in an increase in production.",
+            ],
+            application: "2 ml should be sprayed with one liter of water. Only a single spray keeps the pest away for a long time."
+        }
+    },
+    {
+        id: 15,
+        name: "Mit Magic",
+        img: Mit,
+        activeIngredient: "Bio Plant Stimulant",
+        url: "#",
+        details: {
+            targetCrops: ["Cotton", "Tomato", "Sunflower", "Rose", "Watermelon", "Gheya", "Capsicum"],
+            dose: "100 ml/acre in 100-150 liter water.",
+            pests: ["........"],
+            uses: "It is bio Stimulant, available from agrochemical modification. Mit-Magic: Awakens plants from dormant state by stimulating biological action. Increases production by causing rapid growth, and division of plant cells.",
+            advantages: [
+                "For crops like Capsicum, Tomato, Gheya, Sunflower, Rose, Watermelon, Melon, Cotton, etc.",
+            ],
+            application: "It is very useful for diseases like twisting, tying, Mit-Magic (red zoom), aphids, jasids, etc. Provides strength to fight disease. Quantity: 100 ml Spray Mit-Magic in 150 to 200 liters of water."
+        }
+    }
+]
