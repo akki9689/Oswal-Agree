4940448b53c5b6bab76429a598baaaf57da0c32f
Ashique <ansariazad7864@gmail.com>
2024-08-28 20:35:44 +0530

    Product

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Footer from './components/common/Footer'
 import Branches from './components/contactUs/Branches'
 
 import Navbar from './components/common/Navbar'
-import ProductDetails from './components/common/ProductDetails'
 import Products from './components/product/Products'
 const App = () => {
 
@@ -15,7 +14,7 @@ const App = () => {
         <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-open-sans'>
             <Navbar />
             <Product/>
-            <ProductDetails/>
+            
 
           <Products/>
 
--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import ProductCard from '../../components/common/ProductCard';
+import { allProducts } from '../../data/products/all-products-data'; // Ensure correct import
 
 const Products = () => {
-    return (
-        <div className="grid grid-cols-1 gap-4 p-4 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center" style={{ maxWidth: '1200px' }}>
-            <ProductCard
-                imageSrc="https://via.placeholder.com/150"
-                productName="Fila Plus"
-                productDescription="Alphacypermethrin 10% SC"
-            />
-            <ProductCard
-                imageSrc="https://via.placeholder.com/150"
-                productName="Borohit Plus"
-                productDescription="Alphacypermethrin 10% SC"
-            />
-            <ProductCard
-                imageSrc="https://via.placeholder.com/150"
-                productName="Borohit Plus"
-                productDescription="Alphacypermethrin 10% SC"
-            />
-            <ProductCard
-                imageSrc="https://via.placeholder.com/150"
-                productName="Borohit Plus"
-                productDescription="Alphacypermethrin 10% SC"
-            />
-            {/* Add more ProductCard components here */}
-        </div>
-    );
+  const history = useHistory();
+
+  const handleReadMoreClick = (productId) => {
+    history.push(`/product/${productId}`); // Navigate to the ProductPage with product ID
+  };
+
+  return (
+    <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 md:grid-cols-3">
+      {allProducts.flatMap((category) => 
+        category.productName.map((product) => (
+          <ProductCard
+            key={product.id}
+            imageSrc={product.img}
+            productName={product.name}
+            productDescription={product.activeIngredient}
+            onReadMoreClick={() => handleReadMoreClick(product.id)} // Pass the handler
+          />
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Products;
