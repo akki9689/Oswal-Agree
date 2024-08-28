d8a0792941d0408e72e73ebc6aae6ea636fd1a97
Ashique <ansariazad7864@gmail.com>
2024-08-28 13:20:43 +0530

    add some

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -7,20 +7,24 @@ const ProductCard = ({
     productDescription,
 }) => {
     return (
-        <div className="max-w-xs p-4 duration-300 transform bg-gray-50 rounded-xl hover:shadow-2xl hover:scale-100 hover:shadow-black">
-            <div className="flex justify-center mb-6">
-                <img src={imageSrc} alt={productName} className="object-contain h-40" />
-            </div>
-            <h3 className="text-lg font-semibold text-center text-green-700">
-                {productName}
-            </h3>
-            <p className="text-center text-gray-600">{productDescription}</p>
-            <div className="flex justify-center mt-4">
-                <Button
-                    buttonText="Read More"
-                    showIcon={true}
-                    handlar={() => console.log('Button clicked!')}
-                />
+        <div className="w-full max-w-xs mx-auto">
+            <div className="flex flex-col justify-between p-6 transition duration-300 transform bg-white rounded-xl hover:shadow-2xl hover:scale-105 h-[23rem] hover:shadow-richblack-500 ">
+                <div className="flex justify-center mb-2">
+                    <img src={imageSrc} alt={productName} className="object-contain h-40" />
+                </div>
+                <div className="text-center">
+                    <h3 className="text-lg font-semibold text-green-700">
+                        {productName}
+                    </h3>
+                    <p className="text-gray-600">{productDescription}</p>
+                </div>
+                <div className="flex justify-center mt-4">
+                    <Button
+                        buttonText="Read More"
+                        showIcon={true}
+                        handlar={() => console.log('Button clicked!')}
+                    />
+                </div>
             </div>
         </div>
     );
@@ -30,6 +34,7 @@ ProductCard.propTypes = {
     imageSrc: PropTypes.string.isRequired,
     productName: PropTypes.string.isRequired,
     productDescription: PropTypes.string.isRequired,
+    onReadMoreClick: PropTypes.func.isRequired,
 };
 
 export default ProductCard;
--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -3,7 +3,7 @@ import ProductCard from '../../components/common/ProductCard';
 
 const Products = () => {
     return (
-        <div className="grid grid-cols-1 gap-4 p-4 mx-auto sm:grid-cols-3 md:grid-cols-4" style={{ maxWidth: '1200px' }}>
+        <div className="grid grid-cols-1 gap-4 p-4 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center" style={{ maxWidth: '1200px' }}>
             <ProductCard
                 imageSrc="https://via.placeholder.com/150"
                 productName="Fila Plus"
@@ -29,4 +29,4 @@ const Products = () => {
     );
 };
 
-export default Products
+export default Products;
